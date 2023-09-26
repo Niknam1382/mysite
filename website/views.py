6b9d23e8826d8b97037a32aeab5f7f42c348b26d
Niknam1382 <niknam151412@gmail.com>
2023-09-26 15:01:29 +0330

    added general templates folder
    moved website templates to general folder of templates

--- website/views.py
+++ website/views.py
@@ -19,10 +19,10 @@ def index_view(request) :
     return render(request, 'website/index.html')
 
 def about_view(request) :
-    return render(request, 'about.html')
+    return render(request, 'website/about.html')
 
 # def contact_view(request) :
 #     return HttpResponse ('<h1>Contact</h1>')
 
 def contact_view(request) :
-    return render(request, 'contact.html')
+    return render(request, 'website/contact.html')
