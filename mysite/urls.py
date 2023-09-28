61f7b74627d711b5f3458b6c9f7afb460d0b0cdd
Mohammad Mahdi Niknam <niknam151412@gmail.com>
2023-09-28 11:47:28 +0330

    little change

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('', include('website.urls'))
     # path('blog\', include('blog.urls'))
 ]
+# static ('static','bace / static')
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
