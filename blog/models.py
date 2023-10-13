c6560e8518d16bfcd3e5b837c06da9e391228bf4
Niknam1382 <niknam151412@gmail.com>
2023-10-13 19:34:49 +0330

    The continuation of Django
    تا ارتباط با پایگاه داده از طریق shell

--- blog/models.py
+++ blog/models.py
@@ -2,5 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model) :
+    # image
+    # author
     title = models.CharField(max_length=255)
     content = models.TextField()
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.DateTimeField(null=True) #(خودمون تعریفش میکنیم کی پابلیش بشه)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    
