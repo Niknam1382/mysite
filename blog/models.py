0057496e1200cb58931e9562e7f8a7c96d790619
Niknam1382 <niknam151412@gmail.com>
2023-10-15 20:40:16 +0330

    update admin and models .py

--- blog/models.py
+++ blog/models.py
@@ -14,3 +14,7 @@ class Post(models.Model) :
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True)
     
+# SELECT * FROM Post
+# SELECT * FROM Post WHERE status = 1
+    def __str__ (self) :
+        return '{} - {}'.format(self.title, self.id)
