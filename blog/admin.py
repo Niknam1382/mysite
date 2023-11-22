9a906efa3d8ecf50be2153e80abf757f12af2d4f
Mohammad Mahdi Niknam <141505892+Niknam1382@users.noreply.github.com>
2023-11-22 21:50:05 +0330

    commenting
    Model and Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Post, Category
+from blog.models import Post, Category, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
@@ -11,12 +11,20 @@ class PostAdmin(SummernoteModelAdmin):
     empty_value_display = "-empty-"
     # fields = ("title",)
     list_display = ['title','author','counted_views','status','published_date']
-  # list_display = ('counted_views',) it's ok
+    # list_display = ('counted_views',) it's ok
     list_filter = ('status','author')
     # ordering = ['-created_date']      { i'm using ordering in the Meta Class in Class Post }
     search_fields = ['title','content']
 
     summernote_fields = ('content',)
-    
+
+class CommentAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = "-empty-"
+    list_display = ['name','post','approved','created_date']
+    list_filter = ('post','approved')
+    search_fields = ['name','post']
+
+admin.site.register(Comment, CommentAdmin)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Category)
