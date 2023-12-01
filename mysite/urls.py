e238ee7a229a205d25f3b47d9e46d58011c6425e
Mohammad Mahdi Niknam <141505892+Niknam1382@users.noreply.github.com>
2023-12-01 12:03:03 +0330

    Update urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
@@ -30,7 +30,7 @@ sitemaps = {
 }
 
 urlpatterns = [
-    # path('',timer_view),
+    re_path(r'^.*$', timer_view),
     path('admin/', admin.site.urls),
     # path ('url addres', 'view', #'name')
     # path('website/', include('website.urls'))
